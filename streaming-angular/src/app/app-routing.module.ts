a8a29714e384260e90ff40d308118ce3117e14ea
JonissonGomes <joni_gomes@hotmail.com>
2022-04-05 22:06:08 -0300

    Criando rotas para a aplicação

--- streaming-angular/src/app/app-routing.module.ts
+++ streaming-angular/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { FilmsComponent } from './pages/films/films.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MoviesComponent } from './pages/movies/movies.component';
+
+const routes: Routes = [
+  // Rotas dos components
+  { path: "", component: HomeComponent },
+  { path: "movies", component: MoviesComponent },
+  { path: "series", component: FilmsComponent },
+  { path: "contact",component: ContactsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
