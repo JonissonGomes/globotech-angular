0ab94f6a87cab49146b78f586decf4d0d6e3b4d2
JonissonGomes <joni_gomes@hotmail.com>
2022-04-05 22:05:54 -0300

    Adicionando components ao root

--- streaming-angular/src/app/app.module.ts
+++ streaming-angular/src/app/app.module.ts
@@ -4,9 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MoviesComponent } from './pages/movies/movies.component';
+import { FilmsComponent } from './pages/films/films.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MoviesComponent,
+    FilmsComponent,
+    ContactsComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
